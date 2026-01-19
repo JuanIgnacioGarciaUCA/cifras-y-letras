1b53edefbd50c33992c1c8f5af8e9500eae06624
JuanIgnacioGarciaUCA <ignacio.garcia@uca.es>
2026-01-19 20:59:32 +0100

    fix: set base path

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,10 +79,10 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "registerSW.js",
-    "revision": "3ca0b8505b4bec776b69afdba2768812"
+    "revision": "d3d7f62dec2f63972536b06fbd722bd6"
   }, {
     "url": "index.html",
-    "revision": "0.b440jv0kbqs"
+    "revision": "0.tpsos0kqejs"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
---
+++ dist/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/cifras-y-letras/sw.js', { scope: '/cifras-y-letras/' })})}
