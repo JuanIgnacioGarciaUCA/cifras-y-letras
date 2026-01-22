27f84369cb7b05cf5da09c2c94f516696337b681
JuanIgnacioGarciaUCA <ignacio.garcia@uca.es>
2026-01-22 23:30:50 +0100

    New buttoms 2

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "d3d7f62dec2f63972536b06fbd722bd6"
   }, {
     "url": "index.html",
-    "revision": "0.4nugl51sjfc"
+    "revision": "0.khi2ntlurgc"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
